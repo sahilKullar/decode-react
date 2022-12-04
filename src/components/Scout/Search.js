8dcf1aa16fb13b317f24d304c9e0d30b9807f896
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-04 21:56:53 +0530

    implemented callback handler in order to pass the props up the tree 🌳

--- src/components/Scout/Search.js
+++ src/components/Scout/Search.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Search() {
+export default function Search({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState("");
   const handleChange = (e) => {
-    console.log(e.target.value);
+    setSearchTerm(e.target.value);
+    onSearch(e);
   };
 
   return (
-    <label htmlFor="search">
-      Search:
-      <input type="text" id="search" onChange={handleChange} />
-    </label>
+    <>
+      <label htmlFor="search">
+        Search:
+        <input type="text" id="search" onChange={handleChange} />
+      </label>
+      <p>Searching for {searchTerm}</p>
+    </>
   );
 }
--- src/components/Scout/index.js
+++ src/components/Scout/index.js
@@ -2,29 +2,34 @@ import React from "react";
 import List from "./List";
 import Search from "./Search";
 
-const stories = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
 export default function Scout() {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const handleSearch = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
     <div>
-      <Search />
+      <h1>My Hacker stories</h1>
+      <Search onSearch={handleSearch} />
       <hr />
       <List list={stories} />
     </div>
